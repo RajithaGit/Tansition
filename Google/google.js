546d15b253597e64ca29c664278eb6e079abcd9f
Rajitha Tupakula <rtupaku>
2020-09-02 13:07:05 -0700

    added background image

--- Google/google.js
+++ Google/google.js
@@ -1,5 +1,7 @@
-let button = document.getElementById(id).addEventListener('click', buttonClick);
+let button = document.getElementById(id).addEventListener("click", buttonClick);
 
 function buttonClick(id) {
-  console.log(id === "googlesearch"? "I have submitted" :  "I'm feeling lucky!!"); 
+  console.log(
+    id === "googlesearch" ? "I have submitted" : "I'm feeling lucky!!"
+  );
 }
