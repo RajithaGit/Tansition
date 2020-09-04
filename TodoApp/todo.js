3e6516c30b1e9c612afc10c0d57d1408a5092871
Rajitha Tupakula <rtupaku>
2020-09-04 00:03:36 -0700

    New Todo App

---
+++ TodoApp/todo.js
@@ -0,0 +1,69 @@
+function toDoForm(action) {
+  if (action === "open") {
+    document.getElementById("displayForm").style.display = "block";
+  } else {
+    document.getElementById("displayForm").style.display = "none";
+  }
+}
+
+function addRow(tableID) {
+  let table = document.getElementById(tableID);
+
+  let rowCount = table.rows.length;
+  let row = table.insertRow(rowCount);
+
+  let cell = row.insertCell(0);
+  let element = document.createElement("input");
+  element.type = "checkbox";
+  element.innerHTML = "Complete";
+  cell.appendChild(element);
+
+  let cell1 = row.insertCell(1);
+  let element1 = document.createElement("input");
+  element1.type = "text";
+  element1.name = "Priority";
+  cell1.appendChild(element1);
+
+  let cell2 = row.insertCell(2);
+  let element2 = document.createElement("input");
+  element2.type = "text";
+  element2.name = "ToDo";
+  cell2.appendChild(element2);
+
+  let cell3 = row.insertCell(3);
+  let element3 = document.createElement("input");
+  element3.type = "text";
+  element3.name = "End Date";
+  cell3.appendChild(element3);
+
+  let cell4 = row.insertCell(4);
+  let element4 = document.createElement("input");
+  element4.type = "text";
+  element4.name = "Status";
+  cell4.appendChild(element4);
+
+  let cell5 = row.insertCell(5);
+  let element5 = document.createElement("button");
+  element5.type = "button";
+  element5.innerText = "Delete";
+  element5.id = rowCount + 1;
+  element5.onclick = `deleteRow(${tableID}, ${element5.id})`;
+  cell5.appendChild(element5);
+}
+
+function deleteRow(tableID, rowID) {
+  try {
+    let table = document.getElementById(tableID);
+    table.deleteRow(rowID);
+  } catch (e) {
+    alert(e);
+  }
+}
+
+function deleteAll(tableID) {
+  let table = document.getElementById(tableID);
+
+  for (i = table.rows.length - 1; i > 0; i--) {
+    table.deleteRow(i);
+  }
+}
