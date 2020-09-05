e71e4192fd343fd818bf31c29865d07fb8297d6e
Rajitha Tupakula <rtupaku>
2020-09-04 18:06:35 -0700

    Some css styling

--- TodoApp/todo.js
+++ TodoApp/todo.js
@@ -2,18 +2,12 @@ function toDoForm(action) {
   if (action === "open") {
     document.getElementById("displayForm").style.display = "block";
   } else {
-    let prioritySelection = document.getElementById("priority");
-    console.log(prioritySelection.options[prioritySelection.selectedIndex].text);
-    console.log(document.getElementById("toDoText").value);
-    console.log(document.getElementById("toDoDesc").value);
-    console.log(document.getElementById("date").value);
     document.getElementById("displayForm").style.display = "none";
   }
 }
 
 function addRow(tableID) {
   let table = document.getElementById(tableID);
-
   let rowCount = table.rows.length;
   let row = table.insertRow(rowCount);
   let rowId = rowCount + 1;
@@ -21,7 +15,10 @@ function addRow(tableID) {
   let cell = row.insertCell(0);
   let checkTag = document.createElement("div");
   checkTag.innerHTML =
-    '<input type="checkbox" onClick = "strikeOffToDo(\'todo' +
+    '<input type="checkbox" onClick = "strikeOffToDo(\'toDo' +
+    rowId +
+    "','" +
+    "status" +
     rowId +
     "')\" id=" +
     rowId +
@@ -32,26 +29,31 @@ function addRow(tableID) {
   let prioritySelection = document.getElementById("priority");
   let element1 = document.createElement("INPUT");
   element1.type = "text";
-  element1.value = prioritySelection.options[prioritySelection.selectedIndex].text;
+  element1.value =
+    prioritySelection.options[prioritySelection.selectedIndex].text;
   cell1.appendChild(element1);
 
   let cell2 = row.insertCell(2);
   let element2 = document.createElement("input");
   element2.type = "text";
   element2.value = document.getElementById("toDoText").value;
-  element2.id = "todo" + rowId;
+  element2.id = "toDo" + rowId;
   cell2.appendChild(element2);
+  document.getElementById("toDoText").value = "";
 
   let cell3 = row.insertCell(3);
   let element3 = document.createElement("input");
   element3.type = "text";
-  element3.value = document.getElementById("date").value;
+  let dateValue = (document.getElementById("date").value).split('-');
+  element3.value = dateValue[1]+"/"+dateValue[2]+"/"+dateValue[0];
   cell3.appendChild(element3);
+  document.getElementById("date").value = "";
 
   let cell4 = row.insertCell(4);
   let element4 = document.createElement("input");
   element4.type = "text";
   element4.value = "Pending";
+  element4.id = "status" + rowId;
   cell4.appendChild(element4);
 
   let cell5 = row.insertCell(5);
@@ -61,7 +63,7 @@ function addRow(tableID) {
     tableID +
     "',id)\" id=" +
     rowId +
-    "><img src=\"delete.png\">";
+    '><img src="delete.png">';
   cell5.appendChild(inputTag);
 }
 
@@ -84,7 +86,8 @@ function deleteAll(tableID) {
   }
 }
 
-function strikeOffToDo(todoId) {
-  let toDoText = document.getElementById(todoId);
+function strikeOffToDo(toDoId, statusId) {
+  let toDoText = document.getElementById(toDoId);
   toDoText.style.setProperty("text-decoration", "line-through");
+  document.getElementById(statusId).value = "Done";
 }
