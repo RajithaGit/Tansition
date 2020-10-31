4f11c6a1eeb4c1ab945f1875b20127a1b945e931
Rajitha Tupakula <rtupaku>
2020-10-30 22:58:11 -0700

    css changes

--- milestone-3-project-1/src/client/components/About.jsx
+++ milestone-3-project-1/src/client/components/About.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable arrow-body-style */
 import React from "react";
 
@@ -6,7 +7,7 @@ const AboutUs = () => {
     <>
       <h1 className="mission">Our Mission</h1>
       <p className="para">
-        WebsiteSetup.org is a free online resource for learning web development,
+        <b>VBlog.org</b> is a free online resource for learning web development,
         design and WordPress. Our goal is to help beginners to create their own
         website, blogs and online stores using the right tools and platforms.
       </p>
