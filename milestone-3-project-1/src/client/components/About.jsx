4d0e345ebd90f160d36e8131bd97fbed1f972132
Rajitha Tupakula <rtupaku>
2020-10-30 23:03:45 -0700

    css changes

--- milestone-3-project-1/src/client/components/About.jsx
+++ milestone-3-project-1/src/client/components/About.jsx
@@ -7,9 +7,22 @@ const AboutUs = () => {
     <>
       <h1 className="mission">Our Mission</h1>
       <p className="para">
-        <b>VBlog.org</b> is a free online resource for learning web development,
-        design and WordPress. Our goal is to help beginners to create their own
-        website, blogs and online stores using the right tools and platforms.
+        The best ideas can change who we are. <b>VBlog</b> is where those ideas
+        take shape, take off, and spark powerful conversations. We’re an open
+        platform where 170 million readers come to find insightful and dynamic
+        thinking. Here, expert and undiscovered voices alike dive into the heart
+        of any topic and bring new ideas to the surface. Our purpose is to
+        spread these ideas and deepen understanding of the world.
+      </p>
+      <p className="para">
+        We’re creating a new model for digital publishing. One that supports
+        nuance, complexity, and vital storytelling without giving in to the
+        incentives of advertising. It’s an environment that’s open to everyone
+        but promotes substance and authenticity. And it’s where deeper
+        connections forged between readers and writers can lead to discovery and
+        growth. Together with millions of collaborators, we’re building a
+        trusted and vibrant ecosystem fueled by important ideas and the people
+        who think about them.
       </p>
     </>
   );
